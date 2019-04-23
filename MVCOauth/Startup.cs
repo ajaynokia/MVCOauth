5cba5bfd40df6f0bfb81b60c326c8195fc47214c
Kosuru <ajay_kumar.kosuru.ext@nokia.com>
2019-04-23 15:22:55 +0530

    MVC Oauth Sample

---
+++ MVCOauth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCOauth.Startup))]
+namespace MVCOauth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
